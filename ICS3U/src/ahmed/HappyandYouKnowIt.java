5ff5b3a41689fa316326882d95e45d251b048eab
Ubah Ahmed <ubahahmed771@gmail.com>
2016-04-27 14:23:05 -0400

    If your happy and you know it ....

---
+++ ICS3U/src/ahmed/HappyandYouKnowIt.java
@@ -0,0 +1,22 @@
+package ahmed;
+
+public class HappyandYouKnowIt {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String [] verses = new String [] { "run around", "shoot some hoops", "go and play", "jump around", "stomp around", "go and sleep", "go and eat", "go play games", "go and run", "win the game"};
+		for (int x=0; x<=9; x++)
+		{
+			System.out.println("if your happy and you know it " + verses[x]);
+			System.out.println("if your happy and you know it " + verses[x]);
+			System.out.println("if your happy and you know it and you really want to show it " + verses[x]);
+			System.out.println("if your happy and you know it " + verses[x]);
+			System.out.println("");
+		}
+}
+	
+	
+	
+	
+	
+}
