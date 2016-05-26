b47911047112805f2036678a86909d5187ec8dfd
Ubah Ahmed <ubahahmed771@gmail.com>
2016-05-26 15:00:05 -0400

    Missisipi

---
+++ ICS3U/src/Mississippi.java
@@ -0,0 +1,128 @@
+
+
+public class Mississippi {
+
+	public static void main(String[] args) {
+		// Ubah Ahmed
+		// May 20 2016
+		// missisipi game
+
+
+			/**
+			 * This is the main method. This puts all the letters together
+			 **/
+
+			
+			printM(); //Print the M
+			printI(); //Print the I
+			printS(); //Print the S
+			printS(); //Print the S
+			printI(); //Print the I
+			printS(); //Print the S
+			printS(); //Print the S
+			printI(); //Print the I
+			printP(); //Print the P
+			printP(); //Print the P
+			printI(); //Print the I
+			
+		}
+		
+		
+		public static void printM()
+		{
+			/**
+			 * This is the M method. This creates the M
+			 **/
+			System.out.println("M     M");
+			System.out.println("MM   MM");
+			System.out.println("M M M M");
+			System.out.println("M  M  M");
+			System.out.println("M     M");
+			System.out.println("M     M");
+		}
+		
+		
+		
+		public static void printI()
+		{
+			/**
+			 * This is the I method. This creates the I
+			 **/
+			System.out.println("IIIIIII");
+			System.out.println("   I   ");
+			System.out.println("   I   ");
+			System.out.println("   I   ");
+			System.out.println("   I   ");
+			System.out.println("IIIIIII");
+		}
+		
+		
+		
+		public static void printS()
+		{
+			/**
+			 * This is the S method. This creates the S
+			 **/
+			System.out.println(" SSSSS ");
+			System.out.println("S     S");
+			System.out.println("S      ");
+			System.out.println(" SSSSS ");
+			System.out.println("      S");
+			System.out.println("S     S");
+			System.out.println(" SSSSS ");
+		}
+		
+		public static void printP()
+		{
+			/**
+			 * This is the P method. This creates the P
+			 **/
+			System.out.println("PPPPPP ");
+			System.out.println("P    P ");
+			System.out.println("P    P ");
+			System.out.println("P    P ");
+			System.out.println("P   P  ");
+			System.out.println("PPPP   ");
+			System.out.println("P      ");
+			System.out.println("P      ");
+			System.out.println("P      ");
+			System.out.println("P      ");
+			System.out.println("P      ");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+			
+
+		
+
+	
+
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+
